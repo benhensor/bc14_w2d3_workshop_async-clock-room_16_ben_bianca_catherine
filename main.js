ae1df183080696c862c05181631d3c2c71c42319
CGreen789 <112316339+CGreen789@users.noreply.github.com>
2023-03-22 12:04:16 +0000

    one second so far

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
-const hand = document.querySelector(".hand");
+// We need the second hand of the clock to rotate 6 degrees every second.
+   // 'rotate(6deg)
+   // function rotateHand()
+   // hand.style.transform = `rotate(6
+   // setInterval(rotateHand(), 1000); for 1000ms
+   // 
 
-hand.style.transform = `rotate(90deg)`; //🔍 HINT: THis is an example of how to rotate the hand!
+
+
+// variable for the hand
+const hand = document.querySelector(".hand");
+ // function rotating the hand 6deg
+function rotateHand() {
+    hand.style.transform = `rotate(6deg)`;
+    // console.log('inside function');
+};
+
+setInterval(rotateHand, 1000);
+// console.log('outside');
+//
