e2736be1300a36703e9fa83a9645b3c284ea6bbc
Ben Hensor <benhensor@gmail.com>
2023-03-22 12:30:49 +0000

    main task complete, js date object experimentation

--- main.js
+++ main.js
@@ -9,14 +9,36 @@
 
 // variable for the hand
 const hand = document.querySelector(".hand");
+
+// variable for minute
+const minute = document.querySelector(".minute");
+
  // function rotating the hand 6deg
 let degrees = 0;
-function rotateHand() {
+function rotateSecondHand() {
     degrees+=6;
     hand.style.transform = `rotate(${degrees}deg)`;
     // console.log('inside function');
 };
 
-setInterval(rotateHand, 1000);
+ // function 
+function rotateMinuteHand() {
+    degrees+=6;
+    minute.style.transform = `rotate(${degrees}deg)`;
+    // console.log('inside function');
+};
+
+// call js date object to get realtime seconds get.seconds()
+
+const d = new Date();
+let seconds = d.getSeconds();
+
+
+
+
+setInterval(rotateSecondHand, 1000);
 // console.log('outside');
 //
+setInterval(rotateMinuteHand, 60000);
+
+console.log(seconds);
